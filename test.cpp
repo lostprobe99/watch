a51c1a135933daeea75d461ad9e3b8bc72c3132c
lostprobe99 <lostprobe@qq.com>
2020-09-20 22:34:06 +0800

    added now time display to test.cpp

--- test.cpp
+++ test.cpp
@@ -17,20 +17,22 @@ using namespace probe;
 int main(int argc, char ** argv)
 {
     if(argc == 1)
-        ::printf("\n%s:%s\n\n", __FILE__, __FUNCTION__);
+        ::printf("\n%s:%s: \n", __FILE__, __FUNCTION__);
 
     watch wa;
     wa.start();
     char buf[128]{0};
-    ::std::cout << wa.now(buf, sizeof(buf)) << std::endl;
+    ::printf("watch::now: %s\n\n", wa.now(buf, sizeof(buf)));
     wa.end();
 
-    ::printf("Take %13.6lf ns  for get now time\n", wa.get_ns());
-    ::printf("Take %13.6lf mis for get now time\n", wa.get_mis());
-    ::printf("Take %13.6lf ms  for get now time\n", wa.get_ms());
-    ::printf("Take %13.6lf s   for get now time\n", wa.get_s());
-    ::printf("Take %13.6lf min for get now time\n", wa.get_min());
-    ::printf("Take %13.6lf h   for get now time\n", wa.get_h());
+    ::printf("Take %14.6lf ns  for get now time\n", wa.get_ns());
+    ::printf("Take %14.6lf mis for get now time\n", wa.get_mis());
+    ::printf("Take %14.6lf ms  for get now time\n", wa.get_ms());
+    ::printf("Take %14.6lf s   for get now time\n", wa.get_s());
+    ::printf("Take %14.6lf min for get now time\n", wa.get_min());
+    ::printf("Take %14.6lf h   for get now time\n", wa.get_h());
+
+    ::printf("\nCurrent time is: %s\n", buf);
 
     return 0;
 }
